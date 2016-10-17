efe38ffc7706126afc07db5b60036606cacfd861
Roshan R <roshan.atl@gmail.com>
2016-10-17 18:12:58 +0530

    Update hello.js

--- OCRImageToText-old/src/main/webapp/hello.js
+++ OCRImageToText-old/src/main/webapp/hello.js
@@ -2,7 +2,7 @@ angular.module('demo', [])
 
 
 .controller('Hello', function($scope, $http) {
-$http.get("your_data").success(function (data) 
+$http.get("http://hack-rest.herokuapp.com/bucketLists?groupId=1").success(function (data) 
 { 
    $scope.bucKetLists = data.messages; 
    console.log("users" , $scope.users); 
