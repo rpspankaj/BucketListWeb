2a1290e845ce2ad10ac705adfd7d55d9cadb8b1e
Roshan R <roshan.atl@gmail.com>
2016-10-18 17:50:10 +0530

    Update groups.js

--- OCRImageToText-old/src/main/webapp/BucketListWeb/js/angular/groups.js
+++ OCRImageToText-old/src/main/webapp/BucketListWeb/js/angular/groups.js
@@ -1,1 +1,11 @@
 
+angular.module('GroupApp', [])
+
+
+.controller('GroupController', function($scope, $http) {
+$http.get("https://hack-rest.herokuapp.com/groups?userId=2").success(function (data) 
+{ 
+   $scope.groupList = data; 
+   console.log("groupList" , $scope.groupList); 
+}); 
+});
